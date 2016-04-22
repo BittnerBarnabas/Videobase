a8d169e69759b95f5d3a25550bb3a4381f0dc635
Barnabás Bittner <bittnerbarni@gmail.com>
2016-04-22 19:47:18 +0200

    Commenting

--- Videobase/Videobase/View/mainwindow.h
+++ Videobase/Videobase/View/mainwindow.h
@@ -11,8 +11,7 @@ public:
 	MainWindow(QWidget * parent = Q_NULLPTR);
 	~MainWindow();
 	void populateDBTable();
-
 private:
 	std::unique_ptr<Ui::MainWindow> ui;
-	std::unique_ptr<QSqlRelationalTableModel> tableModel;
+	std::unique_ptr<QSqlRelationalTableModel> tableModel; //!< QT model for connecting SQL and View
 };
